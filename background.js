7b9d8eeda15f4efb31f3ccebd4bdc6ec3b77f70a
DatPieceOfCake12 <81319757+DatPieceOfCake12@users.noreply.github.com>
2024-07-14 18:05:31 -0500

    Implemented Popup Insertion on tab switch and tab refresh.

--- background.js
+++ background.js
@@ -1,5 +1,54 @@
+var urls = [];
+chrome.runtime.onStartup.addListener(function () {
+  fetch(chrome.runtime.getURL("urls.txt"))
+    .then((response) => response.text())
+    .then((data) => {
+      console.log("Contents of urls.txt:", data);
+      urls = data.split("\n");
+    })
+    .catch((error) => {
+      console.error("Error reading urls.txt:", error);
+    });
+});
+
+//same thing but for when it's first installed
+chrome.runtime.onInstalled.addListener(function () {
+  fetch(chrome.runtime.getURL("urls.txt"))
+    .then((response) => response.text())
+    .then((data) => {
+      console.log("Contents of urls.txt:", data);
+      urls = data.split("\n");
+    })
+    .catch((error) => {
+      console.error("Error reading urls.txt:", error);
+    });
+});
+
+chrome.tabs.onUpdated.addListener(function (tabId) {
+  chrome.tabs.get(tabId, function (tab) {
+    console.log("Tab updated:", tab.url);
+    checkUrl(tab.url, tab.id);
+  });
+});
+
 chrome.tabs.onActivated.addListener(function (activeInfo) {
   chrome.tabs.get(activeInfo.tabId, function (tab) {
     console.log("User switched to tab:", tab.url);
+    checkUrl(tab.url, tab.id);
   });
 });
+
+function checkUrl(url, id) {
+  for (let i = 0; i < urls.length; i++) {
+    if (url.includes(urls[i])) {
+      console.log("Match found for url:", url);
+      chrome.scripting.executeScript({
+        target: { tabId: id },
+        files: ["scripts/popup.js"],
+      });
+      break;
+    } else {
+      console.log("No match found");
+    }
+  }
+}
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,3 +1,15 @@
-const article = document.querySelector("body");
-
-// `document.querySelector` may return null if the selector doesn't match anything.
+if (document.getElementById("overlay")) {
+  console.log("Overlay already exists");
+} else {
+  const overlay = document.createElement("div");
+  overlay.id = "overlay";
+  overlay.style.position = "fixed";
+  overlay.style.top = 0;
+  overlay.style.left = 0;
+  overlay.style.width = "100%";
+  overlay.style.height = "100%";
+  overlay.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+  overlay.style.zIndex = 9999;
+  overlay.style.backdropFilter = "blur(10px)";
+  document.body.appendChild(overlay);
+}
